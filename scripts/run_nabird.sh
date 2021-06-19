42a430a810bb17f8c133d8c987fea94147d92fed
ksaito-ut <keisaito@bu.edu>
2021-06-19 19:00:59 -0400

    clean code, edit README

--- scripts/run_nabird.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python $2  --config configs/nabird-train-config_ODA.yaml --source txt/nabird_source.txt --target txt/nabird_unl.txt --gpu $1
--- scripts/run_visda_opda.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python $2  --config configs/visda-train-config_UDA.yaml --source txt/source_list_univ.txt --target txt/target_list_univ.txt --gpu $1
