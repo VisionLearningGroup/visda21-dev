6b08d9600418d5a413d6f13459786a298ea6df87
Samarth Mishra <samarthm@csa1.bu.edu>
2021-06-19 21:30:36 -0400

    Added util scripts to prepare val data

---
+++ data_prep_utils/modify_filelists.py
@@ -0,0 +1,17 @@
+import os
+
+root = os.path.abspath('.')
+
+
+for filelist in os.listdir('val_filelists'):
+    with open(os.path.join('val_filelists', filelist+'1'), 'w') as fout:
+        with open(os.path.join('val_filelists', filelist), 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                print(line.replace('<root>', root), file=fout, end='')
+    os.remove(os.path.join('val_filelists', filelist))
+    os.rename(
+        os.path.join('val_filelists', filelist+'1'), 
+        os.path.join('val_filelists', filelist))
+
+
