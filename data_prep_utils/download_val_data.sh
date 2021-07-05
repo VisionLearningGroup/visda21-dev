f2881bac7371be54b2b5c2366b3742920a4b3e9d
ksaito-ut <keisaito@bu.edu>
2021-07-05 14:46:05 -0400

    fix download script no need to change file path

--- data_prep_utils/download_val_data.sh
+++ data_prep_utils/download_val_data.sh
@@ -6,6 +6,5 @@ echo "Unzipping"
 unzip -qq val_data.zip
 echo "Removing zip"
 rm val_data.zip
-python data_prep_utils/modify_filelists.py
-cat ./val_filelists/imagenet_c_r_o_filelist.txt ./val_filelists/objectnet_filelist.txt > ./val_filelists/objectnet_imagenet_c_r_o_filelist.txt
+#python data_prep_utils/modify_filelists.py
 
