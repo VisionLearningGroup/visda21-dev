b4143fa9dd754138697d051aa01f5fd513cd59ba
ksaito-ut <keisaito@bu.edu>
2021-07-05 10:56:32 -0400

    fix download script

--- data_prep_utils/download_val_data.sh
+++ data_prep_utils/download_val_data.sh
@@ -7,5 +7,5 @@ unzip -qq val_data.zip
 echo "Removing zip"
 rm val_data.zip
 python data_prep_utils/modify_filelists.py
-cat ./val_filelists/*.txt > ./val_filelists/objectnet_imagenet_c_r_o_filelist.txt
+cat ./val_filelists/imagenet_c_r_o_filelist.txt ./val_filelists/objectnet_filelist.txt > ./val_filelists/objectnet_imagenet_c_r_o_filelist.txt
 
