d60a4d4958df815def86bb608809b48d77ef4ee3
Donghyun Kim <donhk@bu.edu>
2021-07-04 22:46:52 -0700

    update config

--- script/ovanet_train.sh
+++ script/ovanet_train.sh
@@ -1,3 +1,10 @@
-python train_ovanet.py --config ./configs/image_to_objectnet.yaml --source_data /research/diva2/donhk/imagenet/ILSVRC2012_train/ --target_data ./val_filelists/objectnet_filelist.txt --multi 0.1
+python train_ovanet.py --config ./configs/image_to_objectnet_imagenet_c_r_o.yaml --source_data /research/diva2/donhk/imagenet/ILSVRC2012_train/ --target_data ./val_filelists/objectnet_c_r_o.txt --multi 0.1
 
-python train_ovanet.py --config ./configs/image_to_imagenet_c_r_o.yaml --source_data /research/diva2/donhk/imagenet/ILSVRC2012_train/ --target_data ./val_filelists/imagenet_c_r_o_filelist.txt --multi 0.1
+python train_ovanet.py --config ./configs/image_to_objectnet_imagenet_c_r_o.yaml --source_data /research/diva2/donhk/imagenet/ILSVRC2012_train/ --target_data ./val_filelists/objectnet_c_r_o.txt --multi 0.05
+
+python train_ovanet.py --config ./configs/image_to_objectnet_imagenet_c_r_o.yaml --source_data /research/diva2/donhk/imagenet/ILSVRC2012_train/ --target_data ./val_filelists/objectnet_c_r_o.txt --multi 0.01
+
+
+python train_ovanet.py --config ./configs/image_to_objectnet_imagenet_c_r_o.yaml --source_data /research/diva2/donhk/imagenet/ILSVRC2012_train/ --target_data ./val_filelists/objectnet_c_r_o.txt --multi 0.02
+
+python train_ovanet.py --config ./configs/image_to_objectnet_imagenet_c_r_o.yaml --source_data /research/diva2/donhk/imagenet/ILSVRC2012_train/ --target_data ./val_filelists/objectnet_c_r_o.txt --multi 0.05
